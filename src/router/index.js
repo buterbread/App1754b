cbee283e7fd0d4f0afe9c3e799ea802f6b5487dc
Farid Sakhyzad <buterbread@gmail.com>
2018-03-14 00:13:47 +0200

    app1754b

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,32 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+'use strict';
 
-Vue.use(Router)
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
 
 // route-level code splitting
-const createListView = id => () => import('../views/CreateListView').then(m => m.default(id))
-const ItemView = () => import('../views/ItemView.vue')
-const UserView = () => import('../views/UserView.vue')
+const createListView = id => () => import('../views/CreateListView').then(m => m.default(id));
+const ItemView = () => import('../views/ItemView.vue');
+const UserView = () => import('../views/UserView.vue');
+const createApp = id => () => import('../views/app1754').then(m => m.default(id));
 
-export function createRouter () {
-  return new Router({
-    mode: 'history',
-    fallback: false,
-    scrollBehavior: () => ({ y: 0 }),
-    routes: [
-      { path: '/top/:page(\\d+)?', component: createListView('top') },
-      { path: '/new/:page(\\d+)?', component: createListView('new') },
-      { path: '/show/:page(\\d+)?', component: createListView('show') },
-      { path: '/ask/:page(\\d+)?', component: createListView('ask') },
-      { path: '/job/:page(\\d+)?', component: createListView('job') },
-      { path: '/item/:id(\\d+)', component: ItemView },
-      { path: '/user/:id', component: UserView },
-      { path: '/', redirect: '/top' }
-    ]
-  })
+export function createRouter() {
+	return new Router({
+		mode: 'history',
+		fallback: false,
+		scrollBehavior: () => ({y: 0}),
+		routes: [
+			{path: '/app1754', component: createApp('app1754')},
+
+			{path: '/top/:page(\\d+)?', component: createListView('top')},
+			{path: '/new/:page(\\d+)?', component: createListView('new')},
+			{path: '/show/:page(\\d+)?', component: createListView('show')},
+			{path: '/ask/:page(\\d+)?', component: createListView('ask')},
+			{path: '/job/:page(\\d+)?', component: createListView('job')},
+			{path: '/item/:id(\\d+)', component: ItemView},
+			{path: '/user/:id', component: UserView},
+			{path: '/', redirect: '/top'}
+		]
+	})
 }
